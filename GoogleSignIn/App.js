fce435c689bd0837d98eee15e37a4757d0ae4ade
Anil <desh.anil@yahoo.com>
2019-05-27 19:55:34 -0500

    adds google signin

--- GoogleSignIn/App.js
+++ GoogleSignIn/App.js
@@ -8,22 +8,55 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
+import config from './config'
 
 type Props = {};
 export default class App extends Component<Props> {
+  async componentDidMount() {
+    this._configureGoogleSignIn();
+    // await this._getCurrentUser();
+  }
+
+  _configureGoogleSignIn() {
+    GoogleSignin.configure({
+      webClientId: config.webClientId,
+      offlineAccess: true,
+      forceConsentPrompt: true,
+    }).then(res=> console.log('GOOGLE_', res)).catch((error)=> console.log('GOOGLE_', error));
+  }
+
+  _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log('GOOGLE_', JSON.stringify(userInfo))
+      this.setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('GOOGLE_','SIGN_IN_CANCELLED');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('GOOGLE_','IN_PROGRESS');
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('GOOGLE_','PLAY_SERVICES_NOT_AVAILABLE');
+        // play services not available or outdated
+      } else {
+        console.log('GOOGLE_', error);
+        // some other error happened
+      }
+    }
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to Google SignIn!</Text>
+        <GoogleSigninButton
+          style={{ width: 192, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={this._signIn}
+          disabled={false} />
       </View>
     );
   }
---
+++ GoogleSignIn/config.js
@@ -0,0 +1,3 @@
+export default {
+  webClientId: '863580447899-ikr3qk5dj2h6jb5gfa88jrfmkjdptm45.apps.googleusercontent.com',
+};
